c4e4d6173f7f476a68362735fe63bf2d7225b414
Crypto137 <Crypto137@users.noreply.github.com>
2024-05-19 22:17:22 +0300

    Implement message enum generator tool

---
+++ tools/MHProtobufTools/MessageEnumGenerator/Generator.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace MessageEnumGenerator
+{
+    internal class Generator
+    {
+        private string _root;
+        private string _protoDirectory;
+
+        public Generator(string directoryName)
+        {
+            _root = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _protoDirectory = Path.Combine(_root, directoryName);
+        }
+
+        public void Generate()
+        {
+            StringBuilder sb = new();
+
+            foreach (string filePath in Directory.GetFiles(_protoDirectory))
+            {
+                if (Path.GetExtension(filePath) != ".proto") continue;
+                ProcessProtobuf(filePath, sb);
+            }
+
+            string outputPath = Path.Combine(_root, "ProtocolEnums.cs");
+            File.WriteAllText(outputPath, sb.ToString());
+            Console.WriteLine($"Saved output to {outputPath}");
+        }
+
+        private void ProcessProtobuf(string filePath, StringBuilder sb)
+        {
+            Console.WriteLine($"Processing {Path.GetFileNameWithoutExtension(filePath)}...");
+
+            string[] proto = File.ReadAllLines(filePath);
+
+            sb.AppendLine($@"public enum {Path.GetFileNameWithoutExtension(filePath)}Message : uint");
+            sb.AppendLine(@"{");
+
+            uint messageId = 0;
+
+            foreach (string line in proto)
+            {
+                if (line.StartsWith("message") == false) continue;
+                sb.AppendLine($"\t{line[8..^2]},");
+                messageId++;
+            }
+
+            sb.AppendLine(@"}");
+            sb.AppendLine();
+
+            Console.WriteLine($"Processed {messageId} messages");
+        }
+    }
+}
---
+++ tools/MHProtobufTools/MessageEnumGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MessageEnumGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string protobufDirectoryName;
+            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter protobuf directory name:");
+                protobufDirectoryName = Console.ReadLine();
+            }
+            else
+            {
+                protobufDirectoryName = args[0];
+            }
+
+            try
+            {
+                Generator generator = new(protobufDirectoryName);
+                generator.Generate();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
